903f666efb3d1f13b1bfd0e831ab62c827befaa8
Kamy <kamy.leeret@gmail.com>
2020-05-07 01:12:25 -0600

    working through index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -23,7 +23,7 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;   ///has where "condition"
 
   console.log("condition", condition);
