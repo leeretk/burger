d3f02d32ca77390a0c80ca522ef9930ddf1c4587
Kamy <kamy.leeret@gmail.com>
2020-05-12 22:46:18 -0600

    heroku is working
    but its not working locally now https://agile-coast-95462.herokuapp.com/

--- config/connection.js
+++ config/connection.js
@@ -1,37 +1,17 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 require("dotenv").config();
-
-var PORT = process.env.PORT || 3006;
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.PORT,
-  user: "root",
-  password: process.env.secret,
-  database: "burgers_db"
-});
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "	op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "rwkdpkqh8y2mkc5z",
-    password: process.env.jawssecret,
-    database: "nvvsvlzge0g0b4yb"
-  });
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
 }
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,17 @@
 ### Schema
 
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-USE burgers_db;
+USE nvvsvlzge0g0b4yb;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT 0,
+    createdAt TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, 
 	PRIMARY KEY (id)
 );
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Bacon Cheeseburger','0'), 
+ ('Mushroom Swiss Cheeseburger','1'), 
+ ('Beefy Cheesy Sliders','0') 
